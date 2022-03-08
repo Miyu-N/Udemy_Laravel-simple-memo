2d3232e492f9fe61af5b7b79ab38be54436b927c
Miyu-N <myu.star1217@gmail.com>
2022-03-08 23:49:16 +0900

    メモ一覧

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Memo;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,23 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        //ここでメモデータを取得
+        $memos = Memo::select('memos.*')
+                        ->where('user_id', '=', \Auth::id())
+                        ->whereNull('deleted_at')
+                        ->orderBy('updated_at', 'desc')
+                        ->get();
+
+        return view('create', compact('memos'));
+    }
+
+    public function store(Request $request)//インスタンス化して使えるように
+    {
+        $posts = $request->all();
+
+        Memo::insert(['content' => $posts['content'], 'user_id' => \Auth::id()]);
+
+        return redirect( route('home') );
     }
 }
