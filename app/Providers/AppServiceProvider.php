199aa6259f1606c244f5b930af6a31324ccd80c3
Miyu-N <myu.star1217@gmail.com>
2022-03-10 00:13:30 +0900

    タグの絞り込み

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,11 +27,27 @@ class AppServiceProvider extends ServiceProvider
     {
         //すべてのメソッドが呼ばれる前に呼ばれるメソッド
         view()->composer('*', function($view) {
-            $memos = Memo::select('memos.*')
+            $query_tag = \Request::query('tag');
+
+            //もしクエリパラメータtagがあれば
+            if( !empty($query_tag)) {
+                //タグで絞り込み
+                $memos = Memo::select('memos.*')
+                        ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
+                        ->where('memo_tags.tag_id', '=', $query_tag)
                         ->where('user_id', '=', \Auth::id())
                         ->whereNull('deleted_at')
                         ->orderBy('updated_at', 'desc')
                         ->get();
+
+            }else{
+                //タグがなければすべて取得
+                $memos = Memo::select('memos.*')
+                            ->where('user_id', '=', \Auth::id())
+                            ->whereNull('deleted_at')
+                            ->orderBy('updated_at', 'desc')
+                            ->get();
+            }
 
             $tags = Tag::where('user_id', '=', \Auth::id())
                         ->whereNull('deleted_at')
